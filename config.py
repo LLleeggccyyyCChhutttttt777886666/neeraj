4c7a536f57f19289f1c59bfb15844fdcd794d3a4
Legacychutt <yoginum2@outlook.com>
2024-12-24 04:15:34 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY 
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7527768602:AAGmoQZQ6G9aLNSnETE0gtos3bZUDJcV0aQ")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@H3X_neeraj")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1216702307, 5004674194")
+
+
+
+
+
+
+
+  
